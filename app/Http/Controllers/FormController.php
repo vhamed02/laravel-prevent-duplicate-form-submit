65d350c2df09b660c9c26f42779a24aff5cae9f5
Hamed Najjari <hamed.najjari@dimensaoglobal.com>
2023-02-02 20:28:07 +0330

    Reject duplicate requests by generating token

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,18 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class FormController extends Controller
 {
     public function show()
     {
+        $submissionToken = Str::random(32);
+        session()->put('submissionToken', $submissionToken);
 
-        return view('form');
+        return view('form', compact('submissionToken'));
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        if($request->submissionToken !== session()->get('submissionToken')) {
+            abort(419);
+        }
+
+        session()->put('submissionToken', Str::random(32));
+
         sleep(1);
         Log::info('Something...');
         return redirect()->route('form');
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,4 +1,5 @@
 <form action="{{ route('store') }}" method="post">
     @csrf
+    <input type="hidden" name="submissionToken" value="{{ $submissionToken }}">
     <input type="submit">
 </form>
